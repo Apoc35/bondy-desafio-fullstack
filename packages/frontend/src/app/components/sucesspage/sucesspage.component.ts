aeb067a343d0a3089dc34e626845506039f77075
Apoc35 <alexv_silveira@outlook.com>
2024-06-01 13:37:03 -0300

    feat: created the frontend using angular, created the pages and added some libraries.

---
+++ packages/frontend/src/app/components/sucesspage/sucesspage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sucesspage',
+  standalone: true,
+  imports: [],
+  templateUrl: './sucesspage.component.html',
+  styleUrl: './sucesspage.component.css'
+})
+export class SucesspageComponent {
+
+}
